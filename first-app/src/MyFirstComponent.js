275ab598ea6724922b4c2cca4aa0183945e2c3ed
sakerYoucef <youcefsaker09@gmail.com>
2024-08-30 18:08:34 +0100

    add companent

--- first-app/src/MyFirstComponent.js
+++ first-app/src/MyFirstComponent.js
@@ -1,8 +1,18 @@
 export default function MyFirstComponent() {
+  const person = {
+    name: "yarob",
+    pernom: "ssa",
+  };
   return (
     <div>
-      <h1>hello hamid</h1>
+      <h1>
+        {SayHello()} hamid {person.name}
+      </h1>
       <p>hhhhhhhhhh</p>
     </div>
   );
+}
+
+function SayHello() {
+  return "hello avec ";
 }
