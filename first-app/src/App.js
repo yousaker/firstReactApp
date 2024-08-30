53c7df83454cd1d44f2a8e3340630f6a07ade5b3
sakerYoucef <youcefsaker09@gmail.com>
2024-08-30 17:48:56 +0100

    first commit

---
+++ first-app/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import logo from "./logo.svg";
+import MyFirstComponent from "./MyFirstComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>saker</h1>
+        <MyFirstComponent />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/MyFirstComponent.js
@@ -0,0 +1,8 @@
+export default function MyFirstComponent() {
+  return (
+    <div>
+      <h1>hello hamid</h1>
+      <p>hhhhhhhhhh</p>
+    </div>
+  );
+}
